96487e977b6b45e4c1e26d3929139208dcaa91b7
Archana Sekar <archanakmhss@gmail.com>
2024-05-28 18:36:49 +0530

    redirect error correct

--- src/pages/redirect.tsx
+++ src/pages/redirect.tsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const clientId = '7a85f758f7328906cfb81dfc27d658286ce44b09ef4bc5eab45eb5e90f0a2655';
+const clientSecret = 'eb34dcbb40d828151a28fdb8a0fb5055d77410fe56986a43208e3db810d9d1f5';
+const redirectUri = 'https://e2ba-2409-40f4-103f-e1b-20bf-e08c-2320-c830.ngrok-free.app/';
+const scope = 'user:uploads:read';
 
 export const redirect = () => {
     const [accessToken, setAccessToken] = useState<string | null>(null);
@@ -42,3 +48,4 @@ export const redirect = () => {
     <div>redirect</div>
   )
 }
+export default redirect;
